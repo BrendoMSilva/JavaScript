c366ed077b1bccbfe2c2368d7002cdd1f301ddf0
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-08-19 21:37:33 -0300

    image
    image add

--- iRocky/js/script.js
+++ iRocky/js/script.js
@@ -2,7 +2,7 @@ function touchRocky() {
     var userName = prompt('Qual é o seu nome?', 'Enter your name here.');
 
     if (userName) {
-        alert('Prazer em conhecer você,' + userNamae + '.');
-        document.getElementById('rcckImg').src = 'rock_happy.png';
+        alert('Prazer em conhecer você,' + userName + '.');
+        document.getElementById('rockImg').src = 'rockhappy.png';
     }
 }
