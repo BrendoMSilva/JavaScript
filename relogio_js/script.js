a737edb81dd26737a67845d2679f7ca1f66d3203
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2021-08-29 10:36:32 -0300

    watch

---
+++ relogio_js/script.js
@@ -0,0 +1,22 @@
+function horaAtual() {
+    camada = document.getElementById('relogio');
+    hoje = new Date();
+    hora = hoje.getHours();
+    
+    if(hora <= 9) {
+        hora = '0' + hora;
+    }
+    
+    minuto = hoje.getMinutes();
+    if(minuto <= 9) {
+        minuto = '0' + minuto;
+    }
+
+    segundo = hoje.getSeconds();
+    if(segundo <= 9) {
+        segundo = '0' + segundo;
+    }
+
+    camada.innerHTML = hora + ':' + minuto + ':' + segundo;
+    setTimeout('horaAtual()', 1000);
+}
